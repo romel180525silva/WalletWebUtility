2d5d83ef758ac58a70c1c466d4ea79a4ef0f4ad5
sromelrey <romel180525silva@gmail.com>
2020-01-17 16:19:34 +0800

    adding login functionallity

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,25 +8,27 @@ namespace WalletWebUtility.App_Start
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-3.4.1.min.js",
+                        "~/Scripts/jquery-3.4.1.intellisense.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/customScripts").Include(
+                        "~/Scripts/main.js",
+                        "~/Scripts/MyScripts.js",
+                        "~/Scripts/modal-loading.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
-                        "~/Scripts/popper.js"));
+                        "~/Scripts/bootstrap*",
+                         "~/Scripts/modernizr-*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+
+
+            //-------------For CSS --------------------
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
+                      "~/Content/bootstrap*",
+                      "~/Content/modal-loading.css",
+                      "~/Content/modal-loading-animate.css",
                       "~/Content/Style.css"));
 
         }
